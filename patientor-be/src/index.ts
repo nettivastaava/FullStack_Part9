56d4b93198c39871d0efaac13ba29d1307d579d7
Valtteri Andström <valtteri.andstrom@gmail.com>
2022-12-01 15:02:08 +0200

    9.11

--- patientor-be/src/index.ts
+++ patientor-be/src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { Request } from 'express';
 import diagnosisRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+import cors = require('cors');
 
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors<Request>());
 
 const PORT = 3001;
 
@@ -12,6 +16,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnosis', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
